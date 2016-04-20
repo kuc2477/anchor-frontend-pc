d12c3ee9bb286a9dd4d897bc146478a487503f00
Ha Junsoo <kuc2477@gmail.com>
2016-04-20 15:07:04 +0900

    add news dashboard

---
+++ app/components/news/DashBoard.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react'
+
+
+export default class DashBoard extends React.Component {
+  render() {
+    return (
+      <div>
+        NOT IMPLEMENTED YET
+      </div>
+    )
+  }
+}
