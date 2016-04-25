a251dcb15f6e79329cae7d0823bfec9117700147
Ha Junsoo <kuc2477@gmail.com>
2016-04-25 17:54:49 +0900

    add basic connection to crossbar router

--- app/modules/autobahn.js
+++ app/modules/autobahn.js
@@ -1,5 +1,29 @@
 import autobahn from 'autobahn'
+import urls from './urls'
+import {
+  ROUTER_REALM,
+  TOPIC_COVER_STARTED,
+  TOPIC_COVER_FINISHED
+} from '../constants/strings'
 
 
-const connection = new autobahn.Connection({
+// connection to crossbar router
+export default const connection = new autobahn.Connection({
+  url: urls.router(),
+  realm: ROUTER_REALM
 })
+
+
+// subscribe router events
+connection.onopen = (session) => {
+  session.subscribe(TOPIC_COVER_STARTED, (args) => {
+    // TODO: NOT IMPLEMENTED YET
+  })
+  session.subscribe(TOPIC_COVER_FINISHED, (args) => {
+    // TODO: NOT IMPLEMENTED YET
+  })
+}
+
+
+// open connection to the crossbar router
+connection.open()
